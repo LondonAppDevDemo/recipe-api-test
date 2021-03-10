2910ea04b21a7a1a1801f2e96a43c75c9cf8aac3
Demo <demo@marks-mbp.mynet>
2021-03-10 16:39:26 +0000

    Updated view

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -10,7 +10,7 @@ from recipe import serializers
 class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     """Manage tags in the database"""
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    permissions_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
 
